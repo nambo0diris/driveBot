e97713ae3b463883e79e70f2cec08332ebf152d5
a.haindrava <a.haindrava@parisnail.ru>
2022-12-09 11:31:55 +0300

    правлю сценарий для удобного получения ссылки на оплату

--- scenes/make_driver_license_scene.ts
+++ scenes/make_driver_license_scene.ts
@@ -613,20 +613,19 @@ getPhoto.action("make_payment", async ctx => {
                             // @ts-ignore
                             await ctx.replyWithHTML("Оплата прошла. Спасибо!");
                             // @ts-ignore
-                            // @ts-ignore
                             await convert_to_jpeg(ctx.wizard.state).then( async () => {
                                 // @ts-ignore
-                                await ctx.replyWithDocument({ source: `/root/driveBot/temp/users/${ctx.message.chat.id}/Полный_разворот_1.jpg` });
+                                await ctx.replyWithDocument({ source: `/root/driveBot/temp/users/${ctx.update.callback_query.from.id}/Полный_разворот_1.jpg` });
                                 // @ts-ignore
-                                await ctx.replyWithDocument({ source: `/root/driveBot/temp/users/${ctx.message.chat.id}/Полный_разворот_2.jpg` });
+                                await ctx.replyWithDocument({ source: `/root/driveBot/temp/users/${ctx.update.callback_query.from.id}/Полный_разворот_2.jpg` });
                                 // @ts-ignore
-                                await ctx.replyWithDocument({ source: `/root/driveBot/temp/users/${ctx.message.chat.id}/Короткая_версия.jpg` });
+                                await ctx.replyWithDocument({ source: `/root/driveBot/temp/users/${ctx.update.callback_query.from.id}/Короткая_версия.jpg` });
                                 // @ts-ignore
                                 if (ctx.wizard.state.type === "ru_eu"){
                                     // @ts-ignore
-                                    await ctx.replyWithDocument({ source: `/root/driveBot/temp/users/${ctx.message.chat.id}/Европейские(на пластик)_1.jpg` });
+                                    await ctx.replyWithDocument({ source: `/root/driveBot/temp/users/${ctx.update.callback_query.from.id}/Европейские(на пластик)_1.jpg` });
                                     // @ts-ignore
-                                    await ctx.replyWithDocument({ source: `/root/driveBot/temp/users/${ctx.message.chat.id}/Европейские(на пластик)_2.jpg` });
+                                    await ctx.replyWithDocument({ source: `/root/driveBot/temp/users/${ctx.update.callback_query.from.id}/Европейские(на пластик)_2.jpg` });
                                 }
                             });
                         }
