62528fe6a4057622db9e37b57600761e5929d7c1
a.haindrava <a.haindrava@parisnail.ru>
2022-12-22 07:59:58 +0300

    сделал одинаковые подписи, одинаковые даты

--- data_generator/get_expire_date.js
+++ data_generator/get_expire_date.js
@@ -5,6 +5,9 @@ export const get_expire_date = () => {
   const get_rand = (length, max, min = 0) => {
     exp_dd = Math.floor(Math.random() * 100);
     if (exp_dd > min && exp_dd < max) {
+      if (exp_dd < 10){
+        exp_dd = "0"+ exp_dd;
+      }
       return exp_dd;
     } else {
       return get_rand(length,max, min);
@@ -13,6 +16,7 @@ export const get_expire_date = () => {
 
   const mm = get_rand(2,12,0)
   const dd = get_rand(2,28,0)
+
 
   return {
     dd,
